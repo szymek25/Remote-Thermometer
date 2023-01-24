e13d39a4bbf5ee433c6e19c6374be62e9a1d3c59
Pawel Szymasnki <pawel.szymanski@edu.wsti.pl>
2023-01-24 10:43:33 +0100

    Change logic related to sending temperature. Instead of sending each 1s, waiting for proper command and then send

--- main/main.ino
+++ main/main.ino
@@ -16,19 +16,33 @@ void setup() {
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(0x76);
   radio.openWritingPipe(0xF0F0F0F0E1LL);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  radio.openReadingPipe(1, pipe);
   radio.enableDynamicPayloads();
   radio.powerUp();
   printf_begin();
 }
 
 void loop() {
-  sensors.requestTemperatures();//Pobranie temperatury czujnika
-  char text[5];
-  float temp = sensors.getTempCByIndex(0);
-  String tempString;
-  tempString = String(temp);
-  strcpy(text, tempString.c_str());
-  Serial.println(text);
-  radio.write(&text, sizeof(text));
+  radio.startListening();
+  char receivedMessage[32] = {0};
+  if(radio.available()) {
+    radio.read(receivedMessage, sizeof(receivedMessage));
+    Serial.println(receivedMessage);
+    radio.stopListening();
+
+    String stringMessage(receivedMessage);
+    if(stringMessage == "GETTEMPERATURE") {
+      sensors.requestTemperatures();//Pobranie temperatury czujnika
+      char text[5];
+      float temp = sensors.getTempCByIndex(0);
+      String tempString;
+      tempString = String(temp);
+      strcpy(text, tempString.c_str());
+      Serial.println(text);
+      radio.write(&text, sizeof(text));
+    }
+  }
+
   delay(1000);
 }
